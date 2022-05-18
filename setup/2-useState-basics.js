5662f3a73761c5ebada9b0c9fc176fbed35b1a56
mmaysam <92256648+mmaysam@users.noreply.github.com>
2022-05-18 23:22:26 +0200

    Add files via upload

---
+++ setup/2-useState-basics.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+// const UseStateBasics = () => {
+//   const [text, setText] = useState('random title ')
+//   const handleClick = () =>{
+//     if (text === 'random title'){
+//         setText("hello ya a7a");
+//     }
+//     else{
+//       setText("random title");
+//     }
+
+
+//   }
+//   return (
+//     <React.Fragment>
+
+//      <h1>{text}</h1>
+//      <button className="btn" onClick={handleClick}>chand title</button>
+//     </React.Fragment>
+    
+
+//   )
+// };
+
+export default UseStateBasics;
